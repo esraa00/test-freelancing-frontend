151c17a4eeaef4bc08e82db6e0d121bdac138da5
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-08-28 04:48:36 +0300

    chore: add authentication route

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import * as authController from "../controllers/auth.controller";
+const router = Router();
+
+router.post("/signup", authController.signup);
+
+export default router;
