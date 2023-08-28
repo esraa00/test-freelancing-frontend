de4613b384268682bc9f59e6fb9be8c23d1bb77b
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-08-28 04:49:21 +0300

    chore: use authentication route + add cors + needed middlewares

---
+++ src/response-handler/db-error.ts
@@ -0,0 +1,5 @@
+// if (err.code === "P2002") {
+//   console.log(
+//     "There is a unique constraint violation, a new user cannot be created with this email"
+//   );
+// }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
-import express, { Request, Response } from "express";
-require("dotenv").config();
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
+import authRoutes from "./routes/auth.route";
+import errorHandling from "./middleware/error-handling.middleware";
+import("../prisma/client");
+import cors from "cors";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello!");
-});
+app.use(cors({}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use(errorHandling);
 
 app.listen(port, () => {
   return console.log(
