db14b601dfb17f4bcf8b139f8501b6cb27d7d770
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-08-30 12:25:16 +0300

    chore: add find by email user service function

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -12,3 +12,20 @@ export const signup = async (
     next(error);
   }
 };
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const accessToken = await authService.login(req.body);
+    res
+      .cookie("accessToken", accessToken, {
+        maxAge: +process.env.USER_ACCESS_TOKEN_COOKIE_EXPIRY_DATE,
+      })
+      .json({ status: 200 });
+  } catch (error) {
+    next(error);
+  }
+};
