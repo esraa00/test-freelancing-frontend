fc312797d8d6aa01a99745ba46a1e3ca72e85169
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-09-02 21:02:24 +0300

    chore: add otp routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import * as authController from "../controllers/auth.controller";
+import isAuthenticated from "../middleware/is-authenticated.middleware";
+
 const router = Router();
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.get("/otp/generate", isAuthenticated, authController.generate);
+router.post("/otp/validate", isAuthenticated, authController.validate);
 
 export default router;
