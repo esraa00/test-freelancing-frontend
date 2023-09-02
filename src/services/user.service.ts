0cacfe8352851db18941b53fccff79b2774d509f
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-09-02 21:00:45 +0300

    chore: add new otp parameters + add find by id funtion

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -7,11 +7,19 @@ export const create = async ({
   lastName,
   email,
   password,
+  otpAscii,
+  otpAuthUrl,
+  otpBase32,
+  otpHex,
 }: {
   firstName: string;
   lastName: string;
   email: string;
   password: string;
+  otpAscii: string;
+  otpBase32: string;
+  otpAuthUrl: string;
+  otpHex: string;
 }) => {
   const createdMessage = await prisma.user.create({
     data: {
@@ -19,6 +27,10 @@ export const create = async ({
       lastName,
       email,
       password,
+      otpAscii,
+      otpBase32,
+      otpAuthUrl,
+      otpHex,
     },
   });
   if (!createdMessage)
@@ -37,6 +49,12 @@ export const updateUser = async (user: Partial<User>, userId: number) => {
 
 export const findByEmail = async (email: string) => {
   const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) throw ApiError.NotFound("user not found");
+  return user;
+};
+
+export const findById = async (id: number) => {
+  const user = await prisma.user.findUnique({ where: { id } });
   if (!user) throw ApiError.NotFound("user not found");
   return user;
 };
