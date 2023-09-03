b836f98c4609ef964fc82ccc455680d2a20f90d0
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-09-03 22:01:42 +0300

    chore: add error handling for jwt

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,20 +1,35 @@
-import jwt from "jsonwebtoken";
+import jwt, { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
+import { ApiError } from "../response-handler/api-error";
 
 export const sign = (
   data: string | object | Buffer,
   secretOrPrivateKey: jwt.Secret,
   options?: jwt.SignOptions | undefined
 ): string => {
-  //TODO: handle error
-  return jwt.sign(data, secretOrPrivateKey, options);
+  try {
+    return jwt.sign(data, secretOrPrivateKey, options);
+  } catch (error) {
+    throw ApiError.Internal("something went wrong, please try again later");
+  }
 };
 
 export const verify = (
+  nameOfToken: string,
   token: string,
   secretOrPublicKey: jwt.Secret,
   options?: jwt.VerifyOptions & {
     complete: true;
   }
 ) => {
-  return jwt.verify(token, secretOrPublicKey, options);
+  try {
+    return jwt.verify(token, secretOrPublicKey, options);
+  } catch (error) {
+    if (error instanceof TokenExpiredError) {
+      throw ApiError.Forbidden(`${nameOfToken} has expired`);
+    } else if (error instanceof JsonWebTokenError) {
+      throw ApiError.Forbidden(`${nameOfToken} is not valid`);
+    } else {
+      throw error;
+    }
+  }
 };
