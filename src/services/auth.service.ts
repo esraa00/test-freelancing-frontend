6d2d60c02450e09676d200f253f68a403658d1dc
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-09-03 22:03:20 +0300

    chore: add error handling for prisma

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,11 +4,12 @@ import bcrypt from "bcrypt";
 import { serializeUser } from "../utils/serialize";
 import { hashEmailVerificationToken } from "./email.service";
 import { ApiError } from "../response-handler/api-error";
-import jwt from "jsonwebtoken";
 import speakeasy from "speakeasy";
+import { Prisma } from "@prisma/client";
+import { sign } from "./jwt.service";
 
 const createAccessToken = (userId: number) => {
-  return jwt.sign({ userId }, process.env.USER_ACCESS_TOKEN_KEY, {
+  return sign({ userId }, process.env.USER_ACCESS_TOKEN_KEY, {
     expiresIn: process.env.USER_ACCESS_TOKEN_EXPIRY_DATE,
   });
 };
@@ -24,30 +25,40 @@ export const signup = async ({
   email: string;
   password: string;
 }) => {
-  const hashedPassword = bcrypt.hashSync(password, 10);
-  var secret = speakeasy.generateSecret({ name: "Your Store Name" });
-  const createdUser = await create({
-    firstName,
-    lastName,
-    email,
-    password: hashedPassword,
-    otpAscii: secret.ascii,
-    otpHex: secret.hex,
-    otpBase32: secret.base32,
-    otpAuthUrl: secret.otpauth_url,
-  });
-  const token = hashEmailVerificationToken(createdUser.id);
-  sendMail({
-    to: createdUser.email,
-    subject: "Hello store Email Verification",
-    from: "em4728644@gmail.com",
-    html: `
+  try {
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    var secret = speakeasy.generateSecret({ name: "Your Store Name" });
+    const createdUser = await create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      otpAscii: secret.ascii,
+      otpHex: secret.hex,
+      otpBase32: secret.base32,
+      otpAuthUrl: secret.otpauth_url,
+    });
+    const token = hashEmailVerificationToken(createdUser.id);
+    sendMail({
+      to: createdUser.email,
+      subject: "Hello store Email Verification",
+      from: "em4728644@gmail.com",
+      html: `
     <h1>Hello Store</h1>
     <p>Please click the following link to verify your email:</p>
     <a href="http://localhost:3000/api/email/confirmation/${token}">Verify Email</a>
     `,
-  });
-  return serializeUser(createdUser);
+    });
+    return serializeUser(createdUser);
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      throw ApiError.BadRequest(
+        "email already exist, please choose another one"
+      );
+    } else {
+      throw error;
+    }
+  }
 };
 
 export const login = async ({
